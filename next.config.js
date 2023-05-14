f01bdd5ccd282cdb6fc9924c609daa04e08ff21b
Suei43 <sueiraphael@gmail.com>
2023-05-14 22:20:48 +0100

    file-based routing test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+  // Tell Next.js to use the "src" directory as the root directory
+  webpack(config, options) {
+    config.resolve.modules.push(path.resolve("./src"));
+    return config;
+  },
+};
---
+++ src/pages/y.js
@@ -0,0 +1,5 @@
+function YPage() {
+    return <h1>Welcome to Y page!</h1>;
+  }
+  
+  export default YPage;
